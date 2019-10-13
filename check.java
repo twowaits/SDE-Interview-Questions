abdb0c53d62d8f7e9874915460f0850d6dd23fa9
aayush7484 <44050528+aayush7484@users.noreply.github.com>
2019-10-13 13:32:53 +0530

    Update check.java

--- check.java
+++ check.java
@@ -1,7 +1,7 @@
 public class check 
 {
-public static void main(String args[])
+public static void main(String []args)
 {
-System.out.println("hello world");
+System.out.println("hello world!");
 }
 }
